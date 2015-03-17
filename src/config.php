a53d2c94e3253067c77e9cc8fef266d88c4227cd
thujeevan <thujee@gmail.com>
2015-03-17 16:42:50 +0530

    updated config to load confs, registered payment providers as services

--- src/config.php
+++ src/config.php
@@ -8,12 +8,14 @@
 
 namespace pr1;
 
-use SebastianBergmann\RecursionContext\Exception;
+use Exception;
+use pr1\api\providers\BTProvider;
+use pr1\api\providers\PPProvider;
 use Silex\Application;
 use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Yaml;
 
 define('ROOT',  dirname(__DIR__));
 
@@ -39,11 +41,32 @@ $app->register(new SessionServiceProvider(), array(
     )
 ));
 
-$app->before(function(Request $request) use ($app){
+try {
+    $file = __DIR__ . '/confs.yml';
+    if (!is_readable($file)) {
+        throw new Exception('Failed to load configuration file');
+    }
+    $conf = file_get_contents($file);
+    $configs = Yaml::parse($conf);
+} catch (Exception $exc) {
+    die('Unable to load configuration file, please verify');
+}
+
+
+$app['ppConf'] = function($app) use ($configs){
+    return $configs['paypal'];
+};
+
+$app['btConf'] = function($app) use ($configs){
+    return $configs['braintree'];
+};
+
+$app['provider.pp'] = $app->share(function($app){
+    return new PPProvider($app['ppConf']);
 });
 
-$app->error(function (Exception $e, $code) use ($app) {
-    
+$app['provider.bt'] = $app->share(function($app){
+    return new BTProvider($app['btConf']);
 });
 
 # binding the routes
