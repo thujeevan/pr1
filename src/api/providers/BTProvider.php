d013d034643688611f4426fccee8c59333cfe444
thujeevan <thujee@gmail.com>
2015-03-17 23:32:40 +0530

    added implementation for processing with braintree gateway

--- src/api/providers/BTProvider.php
+++ src/api/providers/BTProvider.php
@@ -2,6 +2,8 @@
 
 namespace pr1\api\providers;
 
+use Braintree_Configuration;
+use Braintree_Transaction;
 use pr1\api\processor\Processable;
 
 /**
@@ -10,15 +12,51 @@ use pr1\api\processor\Processable;
  * @author Thurairajah Thujeevan
  */
 class BTProvider implements Processable {
+    
+    protected $conf;
+    protected $db;
 
-    protected $apiContext;
-
-    public function __construct($conf) {
-        $this->apiContext = $apiContext;
+    public function __construct($conf, $db) {
+        $this->conf = $conf;
+        $this->db = $db;
+        $this->setupContext($conf);
+    }
+    
+    protected function setupContext($conf) {
+        Braintree_Configuration::environment($conf['credentials']['environment']);
+        Braintree_Configuration::merchantId($conf['credentials']['merchantId']);
+        Braintree_Configuration::publicKey($conf['credentials']['publicKey']);
+        Braintree_Configuration::privateKey($conf['credentials']['privateKey']);
     }
 
     public function process($data) {
-        
+        $result = Braintree_Transaction::sale(
+                        array(
+                            'amount' => $data['price'],
+                            'merchantAccountId' => $this->conf['merchant_accounts'][$data['currency']],
+                            'creditCard' => array(
+                                'number' => preg_replace("/\D|\s/", "", $data['card-number']),
+                                'expirationMonth' => $data['expiry-month'],
+                                'expirationYear' => $data['expiry-year'],
+                                'cvv' => $data['cvv']
+                            ),
+                            'customer' => array(
+                                'firstName' => $data['card-holder-name']
+                            ),
+                            "options" => array(
+                                "submitForSettlement" => true
+                            )
+                        )
+        );
+
+        if ($result->success) {
+            // TODO : persist data 
+            return "Transaction success! \n Transaction Id: " . $result->transaction->id;
+        } else if ($result->transaction) {
+            return FALSE;
+        } else {
+            return FALSE;
+        }
     }
 
 }
