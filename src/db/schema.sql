20bf945d1937454cc074ada49e49b55f5bcdaa34
thujeevan <thujee@gmail.com>
2015-03-17 23:34:36 +0530

    added db script for schema definition

---
+++ src/db/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS pr1orders;
+
+CREATE TABLE `pr1orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `payment_id` varchar(50) DEFAULT NULL,
+  `payment_provider` varchar(10) DEFAULT NULL,
+  `intent` varchar(10) DEFAULT NULL,
+  `payment_method` varchar(10) DEFAULT NULL,
+  `state` varchar(20) DEFAULT NULL,
+  `amount` varchar(20) DEFAULT NULL,
+  `currency` varchar(10) DEFAULT NULL,
+  `description` varchar(40) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
